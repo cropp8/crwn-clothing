93ee413c21b363caffa67c79c8795dee609bbec8
cropp8 <mens.ascendens@gmail.com>
2022-01-20 23:21:59 +0200

    moved directory & shop data into selectors

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import './collections-overview.styles.scss';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {collections.map(({ id, ...collectionProps }) => (
+      <CollectionPreview key={id} {...collectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
