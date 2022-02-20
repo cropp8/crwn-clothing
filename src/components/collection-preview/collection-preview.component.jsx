0be4e96fea795a33211c43676366f5e503732563
cropp8 <mens.ascendens@gmail.com>
2022-02-20 15:02:29 +0200

    add spinner component; add routing on collection-preview component title

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,17 +1,23 @@
+import { withRouter } from 'react-router-dom';
 import './collection-preview.styles.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match }) => (
   <div className='collection-preview'>
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
+    <h1
+      className='title'
+      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
+    >
+      {title.toUpperCase()}
+    </h1>
+    <div className='preview'>
       {items
         .filter((item, i) => i < 4)
         .map((item) => (
-          <CollectionItem key={item.id} item={item}/>
-      ))}
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
